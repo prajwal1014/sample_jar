7b9ee078db9d980a68de08acd482edeca970abb0
prajwal1014 <prajwalpg04@gmail.com>
2025-02-24 18:56:20 +0530

    Update App.java

--- src/main/java/sample/App.java
+++ src/main/java/sample/App.java
@@ -7,5 +7,6 @@ public class App {
     public static void main(String[] args) {
         System.out.println("RCB");
         System.out.println("TEam India");
+        System.out.println("BFC");
     }
 }
